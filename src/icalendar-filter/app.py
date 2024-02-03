737956db294b44f40a245a1bd5d9c73ddb4382cd
pjd199 <89210268+pjd199@users.noreply.github.com>
2024-02-03 15:54:18 +0000

    Added date range and support for recurring events

--- src/icalendar-filter/app.py
+++ src/icalendar-filter/app.py
@@ -1,5 +1,6 @@
 """iCalendar Filter."""
 import icalendar  # type: ignore
+import recurring_ical_events  # type: ignore
 import requests
 from apig_wsgi import make_lambda_handler
 from flask import Flask, Response, abort, request
@@ -17,11 +18,18 @@ def filter_events() -> Response:
     if url is None:
         abort(400, description="URL Parameter 'ics' is missing")
     exclude = request.args.getlist("exclude")
+    start_date = request.args.get("start")
+    end_date = request.args.get("end")
     ics_data = requests.get(url, timeout=60).text
     calendar = icalendar.Calendar.from_ical(ics_data)
+    events = (
+        recurring_ical_events.of(calendar).between(start_date, end_date)
+        if (start_date is not None and end_date is not None)
+        else calendar.walk("VEVENT")
+    )
     calendar.subcomponents = [
         event
-        for event in calendar.walk("VEVENT")
+        for event in events
         if all(
             word.lower() not in str(event.get("SUMMARY")).lower()
             and word.lower() not in str(event.get("DESCRIPTION")).lower()
