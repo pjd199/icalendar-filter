92eb7072bafb2f57438a3158bd33503a535da52d
pjd199 <89210268+pjd199@users.noreply.github.com>
2024-02-04 15:09:17 +0000

    added support for travel merge

--- src/icalendar-filter/app.py
+++ src/icalendar-filter/app.py
@@ -20,13 +20,50 @@ def filter_events() -> Response:
     exclude = request.args.getlist("exclude")
     start_date = request.args.get("start")
     end_date = request.args.get("end")
+    merge_travel = (
+        request.args.get("merge-travel") is not None
+        and str(request.args.get("travel")).lower() == "true"
+    )
     ics_data = requests.get(url, timeout=60).text
     calendar = icalendar.Calendar.from_ical(ics_data)
+
+    # handle the start and end dates
     events = (
         recurring_ical_events.of(calendar).between(start_date, end_date)
         if (start_date is not None and end_date is not None)
         else calendar.walk("VEVENT")
     )
+
+    # merge travel events
+    if merge_travel:
+        travel_ends = {
+            event.get("DTEND"): event
+            for event in events
+            if "travel" in str(event.get("SUMMARY")).lower()
+            and "this event was created by" in event.get("DESCRIPTION").lower()
+        }
+        travel_starts = {
+            event.get("DTSTART"): event
+            for event in events
+            if "travel" in str(event.get("SUMMARY")).lower()
+            and "this event was created by" in event.get("DESCRIPTION").lower()
+        }
+        for event in events.walk():
+            if event["DTSTART"] in travel_ends:
+                event["DTSTART"] = travel_ends[event["DTSTART"]]["DTSTART"]
+            if event["DTEND"] in travel_starts:
+                event["DTEND"] = travel_starts[event["DTEND"]]["DTEND"]
+
+        events = [
+            event
+            for event in events
+            if not (
+                "travel" in str(event.get("SUMMARY")).lower()
+                and "this event was created by" in event.get("DESCRIPTION").lower()
+            )
+        ]
+
+    # filter the keywords
     calendar.subcomponents = [
         event
         for event in events
