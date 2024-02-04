82f100ee4d274863dfc83c0eee1f8132dabe538e
pjd199 <89210268+pjd199@users.noreply.github.com>
2024-02-04 16:09:54 +0000

    bug fix

--- src/icalendar-filter/app.py
+++ src/icalendar-filter/app.py
@@ -36,35 +36,26 @@ def filter_events() -> Response:
 
     # merge travel events
     if merge_travel:
-        travel_ends = {
-            event.get("DTEND"): event
-            for event in events
-            if "travel" in str(event.get("SUMMARY")).lower()
-            and "this event was created by" in event.get("DESCRIPTION").lower()
-        }
-        travel_starts = {
-            event.get("DTSTART"): event
-            for event in events
-            if "travel" in str(event.get("SUMMARY")).lower()
-            and "this event was created by" in event.get("DESCRIPTION").lower()
-        }
-        for event in events.walk():
-            if event["DTSTART"] in travel_ends:
-                event["DTSTART"] = travel_ends[event["DTSTART"]]["DTSTART"]
-            if event["DTEND"] in travel_starts:
-                event["DTEND"] = travel_starts[event["DTEND"]]["DTEND"]
-
-        events = [
+        travel_events = [
             event
             for event in events
-            if not (
-                "travel" in str(event.get("SUMMARY")).lower()
-                and "this event was created by" in event.get("DESCRIPTION").lower()
-            )
+            if event.get("SUMMARY") is not None
+            and "travel" in str(event.get("SUMMARY")).lower()
+            and event.get("DESCRIPTION") is not None
+            and "this event was created by" in event.get("DESCRIPTION").lower()
         ]
+        travel_ends = {event.get("DTEND").dt: event for event in travel_events}
+        travel_starts = {event.get("DTSTART").dt: event for event in travel_events}
+        for event in events:
+            if event["DTSTART"].dt in travel_ends:
+                event["DTSTART"] = (travel_ends[event["DTSTART"].dt])["DTSTART"]
+            if event["DTEND"].dt in travel_starts:
+                event["DTEND"] = (travel_starts[event["DTEND"].dt])["DTEND"]
+
+        events = [event for event in events if event not in travel_events]
 
     # filter the keywords
-    calendar.subcomponents = [
+    events = [
         event
         for event in events
         if all(
@@ -73,4 +64,7 @@ def filter_events() -> Response:
             for word in exclude
         )
     ]
+
+    # update the calendar and send response
+    calendar.subcomponents = events
     return Response(calendar.to_ical(), mimetype="text/calendar")
