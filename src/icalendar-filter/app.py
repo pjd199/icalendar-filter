fb4deb4f2c1cc72d7f4c8a1d659e9f27c8457066
pjd199 <89210268+pjd199@users.noreply.github.com>
2024-09-30 11:34:05 +0000

    update travel handling

--- src/icalendar-filter/app.py
+++ src/icalendar-filter/app.py
@@ -1,4 +1,7 @@
 """iCalendar Filter."""
+
+from re import findall
+
 import icalendar  # type: ignore
 import recurring_ical_events  # type: ignore
 import requests
@@ -20,10 +23,7 @@ def filter_events() -> Response:
     exclude = request.args.getlist("exclude")
     start_date = request.args.get("start")
     end_date = request.args.get("end")
-    merge_travel = (
-        request.args.get("travel") is not None
-        and str(request.args.get("travel")).lower() == "true"
-    )
+    tag_travel = "tag_travel" in request.args
     ics_data = requests.get(url, timeout=60).text
     calendar = icalendar.Calendar.from_ical(ics_data)
 
@@ -34,44 +34,49 @@ def filter_events() -> Response:
         else calendar.walk("VEVENT")
     )
 
-    # merge travel events
-    if merge_travel:
+    # tag travel events
+    if tag_travel:
+        # list all the travel event
         travel_events = [
             event
             for event in events
-            if event.get("SUMMARY") is not None
-            and "travel" in str(event.get("SUMMARY")).lower()
-            and event.get("DESCRIPTION") is not None
-            and "this event was created by" in event.get("DESCRIPTION").lower()
+            if "travel" in event.get("SUMMARY", "").lower()
+            and "this event was created by" in event.get("DESCRIPTION", "").lower()
         ]
-        travel_ends = {event.get("DTEND").dt: event for event in travel_events}
-        travel_starts = {event.get("DTSTART").dt: event for event in travel_events}
+        # create a mapping for all the start and end times for each travel event
+        travel_time_starts = {e["DTSTART"].dt: e for e in travel_events}
+        travel_time_ends = {e["DTEND"].dt: e for e in travel_events}
         for event in events:
-            if event["DTSTART"].dt in travel_ends:
-                event["DTSTART"] = (travel_ends[event["DTSTART"].dt])["DTSTART"]
-            if event["DTEND"].dt in travel_starts:
-                event["DTEND"] = (travel_starts[event["DTEND"].dt])["DTEND"]
+            # extract the tags in the event
+            tags = findall(r"#\w+", event.get("SUMMARY", ""))
+            if tags and len(findall(r",", event.get("LOCATION", ""))) >= 2:
+                # event has tags and location, so look for a travel event
+                # that directly preceeds this event
+                if event["DTSTART"].dt in travel_time_ends:
+                    travel_event = travel_time_ends[event["DTSTART"].dt]
+                    travel_event["SUMMARY"] += " " + " ".join(tags)
+                    # pop travel_event to avoid duplicate processing
+                    travel_time_starts.pop(travel_event["DTSTART"].dt, None)
+                    travel_time_ends.pop(travel_event["DTEND"].dt, None)
+                # look for a travel event that directly follows this event
+                if event["DTEND"].dt in travel_time_starts:
+                    travel_event = travel_time_starts[event["DTEND"].dt]
+                    travel_event["SUMMARY"] += " " + " ".join(tags)
+                    # pop travel_event to avoid duplicate processing
+                    travel_time_starts.pop(travel_event["DTSTART"].dt, None)
+                    travel_time_ends.pop(travel_event["DTEND"].dt, None)
+
+    # filter the keywords
+    if exclude:
         events = [
             event
             for event in events
-            if not (
-                event.get("SUMMARY") is not None
-                and "travel" in str(event.get("SUMMARY")).lower()
-                and event.get("DESCRIPTION") is not None
-                and "this event was created by" in event.get("DESCRIPTION").lower()
+            if all(
+                word.lower() not in str(event.get("SUMMARY")).lower()
+                and word.lower() not in str(event.get("DESCRIPTION")).lower()
+                for word in exclude
             )
         ]
-
-    # filter the keywords
-    events = [
-        event
-        for event in events
-        if all(
-            word.lower() not in str(event.get("SUMMARY")).lower()
-            and word.lower() not in str(event.get("DESCRIPTION")).lower()
-            for word in exclude
-        )
-    ]
 
     # update the calendar and send response
     calendar.subcomponents = events
